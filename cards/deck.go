2caac5b839f49b56aca3f3bf2ea3f3ab236c3818
Ryan Schranz <ryan.schranz@money.co.uk>
2019-01-29 22:17:18 +0000

    Type conversion

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // Which is a slice of strings
@@ -29,4 +32,8 @@ func (d deck) print() {
 
 func deal(d deck, handsize int) (deck, deck) {
 	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
