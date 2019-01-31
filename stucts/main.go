38a34cc030e833be29e19936476f8071ab787ce5
Ryan Schranz <ryan.schranz@money.co.uk>
2019-01-31 22:02:38 +0000

    Structs in structs

--- stucts/main.go
+++ stucts/main.go
@@ -7,6 +7,17 @@ type person struct {
 	lastName  string
 }
 
+type person2 struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 func main() {
 	// This is not a great syntax to use due to the assignment happening in order
 	// if you change the order of the assignment in the struct then all initialisations of the struct will now be wrong
@@ -32,4 +43,15 @@ func main() {
 	ryan3.firstName = "Ryan"
 	ryan3.lastName = "Schranz"
 	fmt.Printf("%+v", ryan3)
+
+	// Create a new person with their contact info
+	ryan4 := person2{
+		firstName: "Ryan",
+		lastName:  "Schranz",
+		contact: contactInfo{
+			email:   "ryan@schranz.com",
+			zipCode: 94000,
+		},
+	}
+	fmt.Printf("%+v", ryan4)
 }
