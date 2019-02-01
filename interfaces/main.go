449809577b34f3219a39a9a08d1d2edcf6bf8c5c
Ryan Schranz <ryan.schranz@money.co.uk>
2019-02-01 17:07:41 +0000

    Interface notes

--- interfaces/main.go
+++ interfaces/main.go
@@ -39,3 +39,16 @@ func (englishBot) getGreeting() string {
 func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
+
+// Type is used a lot, here is some more information
+// Concrete types: map, struct, int, string.
+// Concrete types can be used and meddled with and used to create a value
+// Interface type cannot be used to create a value
+// Interfaces are not generic types
+// Interfaces are implicit - We do not need to declare what uses the interface
+// Interfaces are a contract to help us manage types
+//  Garbage in -> Garbage out, if our custom types implementation of a function
+//  is broken then interfaces wont help us
+// Interfaces are tough, Step #1 is understanding how to read them
+//  Understand how to read interfaces in the standard lib. Writting your
+//  own interfaces is tough and requires experience
