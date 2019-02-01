5665cde394e5f16b025e4017a5ceec1fcbba8898
Ryan Schranz <ryan.schranz@money.co.uk>
2019-02-01 10:44:50 +0000

    The start of pointers in go

--- stucts/main.go
+++ stucts/main.go
@@ -11,6 +11,8 @@ type person2 struct {
 	firstName string
 	lastName  string
 	contact   contactInfo
+	// You do not need to specify a property name for a nested struct
+	contactInfo
 }
 
 type contactInfo struct {
@@ -52,6 +54,33 @@ func main() {
 			email:   "ryan@schranz.com",
 			zipCode: 94000,
 		},
+		// If no property name if given for a struct, we need to use the struct name as the property name
+		contactInfo: contactInfo{
+			email:   "ryan.schranz@schranz.com",
+			zipCode: 95000,
+		},
 	}
-	fmt.Printf("%+v", ryan4)
+	// This will not update the firstName due to nothing being returned
+	// This is because the pointer is not being updated
+	ryan4.updateName("RyanS")
+
+	// To make sure we update the correct point and memory section
+	// We need to create a pointer reference for ryan4
+	ryan4Pointer := &ryan4
+	ryan4Pointer.updateName2("RyanS")
+	ryan4.print()
+}
+
+// Receiver function for the struct of person2
+func (p person2) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person2) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+// To make a receiver function update the correct pointer it needs to be changed to the following
+func (pointerToPerson2 *person2) updateName2(newFirstName string) {
+	(*pointerToPerson2).firstName = newFirstName
 }
