63e1122fdc41d0a84461e1fe482d6ec9043b743c
Ryan Schranz <ryan.schranz@money.co.uk>
2019-02-01 17:00:56 +0000

    More advanced interface creation

--- interfaces/main.go
+++ interfaces/main.go
@@ -6,6 +6,17 @@ type bot interface {
 	getGreeting() string
 }
 
+// This is a test interface to show a more advanced use
+type botAdvanced interface {
+	// This interface contract requires arguments of string and int and will return string and error
+	getAdvancedGreeting(string, int) (string, error)
+	// This contract will return a type of float 64
+	getBotVersion() float64
+	// This contact will accept an argument of user and return a type of string
+	respondToUser(user) string
+}
+
+type user struct{}
 type englishBot struct{}
 type spanishBot struct{}
 
