2889a7aae54f8600cf091ca2b1d05bf77cf90de8
Ryan Schranz <ryan.schranz@money.co.uk>
2019-02-04 18:48:27 +0000

    Use io.Copy to access the Writer interface while passing in the a function that implements the Reader interface

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -16,11 +17,16 @@ func main() {
 
 	// Make us a built in function of go that creates a variable of type with
 	// a determined N value of of elements
-	bs := make([]byte, 99999)
+	// bs := make([]byte, 99999)
 
 	// The read function does not expand the byte slice, so it is work
 	// always creating a larger byte slice than is needed
-	bw, err := resp.Body.Read(bs)
+	// bw, err := resp.Body.Read(bs)
 
-	fmt.Println(string(bs), bw)
+	// fmt.Println(string(bs), bw)
+
+	// We can condense the output of the above to the terminal
+	// This works because the io.copy function uses the writer interface to connect
+	// to the os.stdout
+	io.Copy(os.Stdout, resp.Body)
 }
