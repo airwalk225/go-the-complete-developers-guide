33b92f5aed51f323c68c236ce5b59e5a82484f6a
Ryan Schranz <ryan.schranz@money.co.uk>
2019-02-10 19:54:12 +0000

    More information about go routines and channels

--- channels/main.go
+++ channels/main.go
@@ -14,6 +14,18 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// By default go will not wait for additional Go routines to finish
+	// We have to use a Channel to keep the main the Go routine open,
+	// or to pass information around between Go routines
+	c := make(chan string)
+
+	// To send messages through the channel
+	// channel <- 5 - Send the value of 5 into this channel
+	// myNumber <- channel - Wait for the value to be sent into the channel
+	//                       When we get one, assign the value to myNumber
+	// fmt.print(<- channel) - Wait for the value to be sent into the channel.
+	//                         When we get one, log it out immediately
+
 	for _, l := range links {
 		// Using the go key word, we can start a new go routine
 		// By default Go will only attempt to use one core
@@ -24,11 +36,11 @@ func main() {
 		// If one thread blocks, another one is picked up and worked on
 		// Parallelism - Multiple threads executed at the exact same time.
 		// Requires multiple CPU's
-		go checkLink(l)
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
